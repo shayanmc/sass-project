d1f02df968f526ce16ec1ff249acab1d8240e457
shayanmc <shayanmc.dev@gmail.com>
2026-02-23 09:14:21 +0330

    add navbar shadow

--- js/app.js
+++ js/app.js
@@ -4,3 +4,18 @@ const navToggle = document.querySelector("#nav-toggle");
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("show-menu");
 });
+
+// function scrollHeader() {
+//   const nav = document.getElementById('header')
+//   if(this.scrollY >= 1) nav.classList.add('scroll-header'); else nav.classList('header')
+// }
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  if (window.scrollY >= 80) {
+    nav.classList.add('scroll-header');
+  } else {
+    nav.classList.remove('scroll-header');
+  }
+}
+
+window.addEventListener('scroll', scrollHeader);
